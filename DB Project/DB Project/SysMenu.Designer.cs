b6cf8325d317d7805d650e9061e325964d3b8720
Reem Osama <32741801+ReemOsama99@users.noreply.github.com>
2020-04-10 02:08:19 +0200

    Project start uploaded

---
+++ DB Project/DB Project/SysMenu.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace DB_Project
+{
+    partial class SysMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_custMenutitle = new System.Windows.Forms.Label();
+            this.btn_sysAddItem = new System.Windows.Forms.Button();
+            this.btn_sysAddBranch = new System.Windows.Forms.Button();
+            this.btn_UpdateCat = new System.Windows.Forms.Button();
+            this.btn_updateBranch = new System.Windows.Forms.Button();
+            this.btn_deleteEmp = new System.Windows.Forms.Button();
+            this.btn_deleteBranch = new System.Windows.Forms.Button();
+            this.lbl_sysAdd = new System.Windows.Forms.Label();
+            this.lbl_sysUpdate = new System.Windows.Forms.Label();
+            this.lbl_sysDelete = new System.Windows.Forms.Label();
+            this.btn_custMenuback = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_custMenutitle
+            // 
+            this.lbl_custMenutitle.Font = new System.Drawing.Font("Book Antiqua", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_custMenutitle.Location = new System.Drawing.Point(377, 45);
+            this.lbl_custMenutitle.Name = "lbl_custMenutitle";
+            this.lbl_custMenutitle.Size = new System.Drawing.Size(229, 77);
+            this.lbl_custMenutitle.TabIndex = 3;
+            this.lbl_custMenutitle.Text = "Menu";
+            // 
+            // btn_sysAddItem
+            // 
+            this.btn_sysAddItem.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_sysAddItem.Location = new System.Drawing.Point(177, 144);
+            this.btn_sysAddItem.Name = "btn_sysAddItem";
+            this.btn_sysAddItem.Size = new System.Drawing.Size(263, 59);
+            this.btn_sysAddItem.TabIndex = 4;
+            this.btn_sysAddItem.Text = "Add Item";
+            this.btn_sysAddItem.UseVisualStyleBackColor = true;
+            // 
+            // btn_sysAddBranch
+            // 
+            this.btn_sysAddBranch.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_sysAddBranch.Location = new System.Drawing.Point(553, 144);
+            this.btn_sysAddBranch.Name = "btn_sysAddBranch";
+            this.btn_sysAddBranch.Size = new System.Drawing.Size(263, 59);
+            this.btn_sysAddBranch.TabIndex = 5;
+            this.btn_sysAddBranch.Text = "Add Branch";
+            this.btn_sysAddBranch.UseVisualStyleBackColor = true;
+            // 
+            // btn_UpdateCat
+            // 
+            this.btn_UpdateCat.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_UpdateCat.Location = new System.Drawing.Point(177, 250);
+            this.btn_UpdateCat.Name = "btn_UpdateCat";
+            this.btn_UpdateCat.Size = new System.Drawing.Size(263, 59);
+            this.btn_UpdateCat.TabIndex = 6;
+            this.btn_UpdateCat.Text = "Update Category";
+            this.btn_UpdateCat.UseVisualStyleBackColor = true;
+            // 
+            // btn_updateBranch
+            // 
+            this.btn_updateBranch.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_updateBranch.Location = new System.Drawing.Point(553, 250);
+            this.btn_updateBranch.Name = "btn_updateBranch";
+            this.btn_updateBranch.Size = new System.Drawing.Size(263, 59);
+            this.btn_updateBranch.TabIndex = 7;
+            this.btn_updateBranch.Text = "Update Branch";
+            this.btn_updateBranch.UseVisualStyleBackColor = true;
+            // 
+            // btn_deleteEmp
+            // 
+            this.btn_deleteEmp.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_deleteEmp.Location = new System.Drawing.Point(177, 365);
+            this.btn_deleteEmp.Name = "btn_deleteEmp";
+            this.btn_deleteEmp.Size = new System.Drawing.Size(263, 59);
+            this.btn_deleteEmp.TabIndex = 8;
+            this.btn_deleteEmp.Text = "Delete Employee";
+            this.btn_deleteEmp.UseVisualStyleBackColor = true;
+            // 
+            // btn_deleteBranch
+            // 
+            this.btn_deleteBranch.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_deleteBranch.Location = new System.Drawing.Point(553, 365);
+            this.btn_deleteBranch.Name = "btn_deleteBranch";
+            this.btn_deleteBranch.Size = new System.Drawing.Size(263, 59);
+            this.btn_deleteBranch.TabIndex = 9;
+            this.btn_deleteBranch.Text = "Delete Branch";
+            this.btn_deleteBranch.UseVisualStyleBackColor = true;
+            // 
+            // lbl_sysAdd
+            // 
+            this.lbl_sysAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_sysAdd.Location = new System.Drawing.Point(26, 161);
+            this.lbl_sysAdd.Name = "lbl_sysAdd";
+            this.lbl_sysAdd.Size = new System.Drawing.Size(116, 42);
+            this.lbl_sysAdd.TabIndex = 10;
+            this.lbl_sysAdd.Text = "Add:";
+            this.lbl_sysAdd.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lbl_sysUpdate
+            // 
+            this.lbl_sysUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_sysUpdate.Location = new System.Drawing.Point(12, 267);
+            this.lbl_sysUpdate.Name = "lbl_sysUpdate";
+            this.lbl_sysUpdate.Size = new System.Drawing.Size(146, 42);
+            this.lbl_sysUpdate.TabIndex = 11;
+            this.lbl_sysUpdate.Text = "Update:";
+            // 
+            // lbl_sysDelete
+            // 
+            this.lbl_sysDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_sysDelete.Location = new System.Drawing.Point(12, 372);
+            this.lbl_sysDelete.Name = "lbl_sysDelete";
+            this.lbl_sysDelete.Size = new System.Drawing.Size(146, 42);
+            this.lbl_sysDelete.TabIndex = 12;
+            this.lbl_sysDelete.Text = "Delete:";
+            // 
+            // btn_custMenuback
+            // 
+            this.btn_custMenuback.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_custMenuback.Location = new System.Drawing.Point(364, 455);
+            this.btn_custMenuback.Name = "btn_custMenuback";
+            this.btn_custMenuback.Size = new System.Drawing.Size(225, 51);
+            this.btn_custMenuback.TabIndex = 17;
+            this.btn_custMenuback.Text = "Back";
+            this.btn_custMenuback.UseVisualStyleBackColor = true;
+            this.btn_custMenuback.Click += new System.EventHandler(this.btn_custMenuback_Click);
+            // 
+            // SysMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(901, 518);
+            this.Controls.Add(this.btn_custMenuback);
+            this.Controls.Add(this.lbl_sysDelete);
+            this.Controls.Add(this.lbl_sysUpdate);
+            this.Controls.Add(this.lbl_sysAdd);
+            this.Controls.Add(this.btn_deleteBranch);
+            this.Controls.Add(this.btn_deleteEmp);
+            this.Controls.Add(this.btn_updateBranch);
+            this.Controls.Add(this.btn_UpdateCat);
+            this.Controls.Add(this.btn_sysAddBranch);
+            this.Controls.Add(this.btn_sysAddItem);
+            this.Controls.Add(this.lbl_custMenutitle);
+            this.Name = "SysMenu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SysMenu";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_custMenutitle;
+        private System.Windows.Forms.Button btn_sysAddItem;
+        private System.Windows.Forms.Button btn_sysAddBranch;
+        private System.Windows.Forms.Button btn_UpdateCat;
+        private System.Windows.Forms.Button btn_updateBranch;
+        private System.Windows.Forms.Button btn_deleteEmp;
+        private System.Windows.Forms.Button btn_deleteBranch;
+        private System.Windows.Forms.Label lbl_sysAdd;
+        private System.Windows.Forms.Label lbl_sysUpdate;
+        private System.Windows.Forms.Label lbl_sysDelete;
+        private System.Windows.Forms.Button btn_custMenuback;
+    }
+}
---
+++ DB Project/DB Project/sysLoginForm.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace DB_Project
+{
+    partial class sysLoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_sysLoginTitle = new System.Windows.Forms.Label();
+            this.lbl_sysUsername = new System.Windows.Forms.Label();
+            this.txt_sysUsername = new System.Windows.Forms.TextBox();
+            this.txt_sysPassword = new System.Windows.Forms.TextBox();
+            this.lbl_sysPassword = new System.Windows.Forms.Label();
+            this.btn_sysLoginback = new System.Windows.Forms.Button();
+            this.btn_sysLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_sysLoginTitle
+            // 
+            this.lbl_sysLoginTitle.Font = new System.Drawing.Font("Book Antiqua", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_sysLoginTitle.Location = new System.Drawing.Point(344, 50);
+            this.lbl_sysLoginTitle.Name = "lbl_sysLoginTitle";
+            this.lbl_sysLoginTitle.Size = new System.Drawing.Size(219, 71);
+            this.lbl_sysLoginTitle.TabIndex = 9;
+            this.lbl_sysLoginTitle.Text = "Login";
+            // 
+            // lbl_sysUsername
+            // 
+            this.lbl_sysUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_sysUsername.Location = new System.Drawing.Point(107, 175);
+            this.lbl_sysUsername.Name = "lbl_sysUsername";
+            this.lbl_sysUsername.Size = new System.Drawing.Size(168, 35);
+            this.lbl_sysUsername.TabIndex = 10;
+            this.lbl_sysUsername.Text = "Username";
+            this.lbl_sysUsername.Click += new System.EventHandler(this.lbl_username_Click);
+            // 
+            // txt_sysUsername
+            // 
+            this.txt_sysUsername.Location = new System.Drawing.Point(354, 172);
+            this.txt_sysUsername.Multiline = true;
+            this.txt_sysUsername.Name = "txt_sysUsername";
+            this.txt_sysUsername.Size = new System.Drawing.Size(256, 38);
+            this.txt_sysUsername.TabIndex = 11;
+            // 
+            // txt_sysPassword
+            // 
+            this.txt_sysPassword.Location = new System.Drawing.Point(354, 300);
+            this.txt_sysPassword.Multiline = true;
+            this.txt_sysPassword.Name = "txt_sysPassword";
+            this.txt_sysPassword.Size = new System.Drawing.Size(256, 38);
+            this.txt_sysPassword.TabIndex = 13;
+            // 
+            // lbl_sysPassword
+            // 
+            this.lbl_sysPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_sysPassword.Location = new System.Drawing.Point(107, 303);
+            this.lbl_sysPassword.Name = "lbl_sysPassword";
+            this.lbl_sysPassword.Size = new System.Drawing.Size(148, 35);
+            this.lbl_sysPassword.TabIndex = 14;
+            this.lbl_sysPassword.Text = "Password";
+            // 
+            // btn_sysLoginback
+            // 
+            this.btn_sysLoginback.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_sysLoginback.Location = new System.Drawing.Point(79, 413);
+            this.btn_sysLoginback.Name = "btn_sysLoginback";
+            this.btn_sysLoginback.Size = new System.Drawing.Size(256, 65);
+            this.btn_sysLoginback.TabIndex = 15;
+            this.btn_sysLoginback.Text = "Back";
+            this.btn_sysLoginback.UseVisualStyleBackColor = true;
+            this.btn_sysLoginback.Click += new System.EventHandler(this.btn_sysLoginback_Click);
+            // 
+            // btn_sysLogin
+            // 
+            this.btn_sysLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_sysLogin.Location = new System.Drawing.Point(569, 413);
+            this.btn_sysLogin.Name = "btn_sysLogin";
+            this.btn_sysLogin.Size = new System.Drawing.Size(256, 65);
+            this.btn_sysLogin.TabIndex = 16;
+            this.btn_sysLogin.Text = "Log in";
+            this.btn_sysLogin.UseVisualStyleBackColor = true;
+            this.btn_sysLogin.Click += new System.EventHandler(this.btn_sysLogin_Click);
+            // 
+            // sysLoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(901, 518);
+            this.Controls.Add(this.btn_sysLogin);
+            this.Controls.Add(this.btn_sysLoginback);
+            this.Controls.Add(this.lbl_sysPassword);
+            this.Controls.Add(this.txt_sysPassword);
+            this.Controls.Add(this.txt_sysUsername);
+            this.Controls.Add(this.lbl_sysUsername);
+            this.Controls.Add(this.lbl_sysLoginTitle);
+            this.Name = "sysLoginForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "sysLoginForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_sysLoginTitle;
+        private System.Windows.Forms.Label lbl_sysUsername;
+        private System.Windows.Forms.TextBox txt_sysUsername;
+        private System.Windows.Forms.TextBox txt_sysPassword;
+        private System.Windows.Forms.Label lbl_sysPassword;
+        private System.Windows.Forms.Button btn_sysLoginback;
+        private System.Windows.Forms.Button btn_sysLogin;
+    }
+}
