fb702a599d4d621e0158bf7839af0944a2379317
NayeraMohamed <32566231+NayeraMohamed@users.noreply.github.com>
2020-04-10 05:51:39 +0200

    bgrb
    7atet //

--- DB Project/DB Project/Program.cs
+++ DB Project/DB Project/Program.cs
@@ -14,6 +14,7 @@ namespace DB_Project
         [STAThread]
         static void Main()
         {
+            //
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainPage());
