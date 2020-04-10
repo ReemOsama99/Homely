b22a96302acc842bb9743191320f0f6d1f46ad5b
NayeraMohamed <32566231+NayeraMohamed@users.noreply.github.com>
2020-04-10 05:52:04 +0200

    Update Program.cs

--- DB Project/DB Project/Program.cs
+++ DB Project/DB Project/Program.cs
@@ -14,7 +14,7 @@ namespace DB_Project
         [STAThread]
         static void Main()
         {
-            ///////
+            ///
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainPage());
