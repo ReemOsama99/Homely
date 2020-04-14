666c2cfb90d15ac073abbcb09c58d8cbdbad470f
maryamabubkr <47347291+maryamabubkr@users.noreply.github.com>
2020-04-14 21:50:04 +0200

    trial

--- DB Project/DB Project/AddCategory.Designer.cs
+++ DB Project/DB Project/AddCategory.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AddCategory));
             this.lbl_AddCattitle = new System.Windows.Forms.Label();
             this.txt_addCatID = new System.Windows.Forms.TextBox();
             this.txt_addCatName = new System.Windows.Forms.TextBox();
@@ -39,76 +40,91 @@
             // 
             // lbl_AddCattitle
             // 
+            this.lbl_AddCattitle.BackColor = System.Drawing.Color.Transparent;
             this.lbl_AddCattitle.Font = new System.Drawing.Font("Book Antiqua", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_AddCattitle.Location = new System.Drawing.Point(293, 54);
+            this.lbl_AddCattitle.Location = new System.Drawing.Point(11, 18);
+            this.lbl_AddCattitle.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbl_AddCattitle.Name = "lbl_AddCattitle";
-            this.lbl_AddCattitle.Size = new System.Drawing.Size(386, 75);
+            this.lbl_AddCattitle.Size = new System.Drawing.Size(290, 61);
             this.lbl_AddCattitle.TabIndex = 4;
             this.lbl_AddCattitle.Text = "Add Category";
             // 
             // txt_addCatID
             // 
             this.txt_addCatID.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txt_addCatID.Location = new System.Drawing.Point(322, 191);
+            this.txt_addCatID.Location = new System.Drawing.Point(242, 155);
+            this.txt_addCatID.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txt_addCatID.Multiline = true;
             this.txt_addCatID.Name = "txt_addCatID";
-            this.txt_addCatID.Size = new System.Drawing.Size(256, 38);
+            this.txt_addCatID.Size = new System.Drawing.Size(193, 32);
             this.txt_addCatID.TabIndex = 12;
             // 
             // txt_addCatName
             // 
             this.txt_addCatName.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txt_addCatName.Location = new System.Drawing.Point(322, 340);
+            this.txt_addCatName.Location = new System.Drawing.Point(242, 246);
+            this.txt_addCatName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txt_addCatName.Multiline = true;
             this.txt_addCatName.Name = "txt_addCatName";
-            this.txt_addCatName.Size = new System.Drawing.Size(256, 38);
+            this.txt_addCatName.Size = new System.Drawing.Size(193, 32);
             this.txt_addCatName.TabIndex = 13;
             // 
             // lbl_AddCatName
             // 
+            this.lbl_AddCatName.BackColor = System.Drawing.Color.Transparent;
             this.lbl_AddCatName.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_AddCatName.Location = new System.Drawing.Point(33, 340);
+            this.lbl_AddCatName.Location = new System.Drawing.Point(25, 246);
+            this.lbl_AddCatName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbl_AddCatName.Name = "lbl_AddCatName";
-            this.lbl_AddCatName.Size = new System.Drawing.Size(223, 35);
+            this.lbl_AddCatName.Size = new System.Drawing.Size(167, 28);
             this.lbl_AddCatName.TabIndex = 14;
             this.lbl_AddCatName.Text = "Category Name";
             // 
             // lbl_addCatID
             // 
+            this.lbl_addCatID.BackColor = System.Drawing.Color.Transparent;
             this.lbl_addCatID.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_addCatID.Location = new System.Drawing.Point(56, 181);
+            this.lbl_addCatID.Location = new System.Drawing.Point(25, 155);
+            this.lbl_addCatID.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbl_addCatID.Name = "lbl_addCatID";
-            this.lbl_addCatID.Size = new System.Drawing.Size(168, 35);
+            this.lbl_addCatID.Size = new System.Drawing.Size(126, 28);
             this.lbl_addCatID.TabIndex = 15;
             this.lbl_addCatID.Text = "CatID";
             // 
             // btn_AddCat
             // 
+            this.btn_AddCat.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.btn_AddCat.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_AddCat.Location = new System.Drawing.Point(623, 441);
+            this.btn_AddCat.ForeColor = System.Drawing.Color.Yellow;
+            this.btn_AddCat.Location = new System.Drawing.Point(467, 358);
+            this.btn_AddCat.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_AddCat.Name = "btn_AddCat";
-            this.btn_AddCat.Size = new System.Drawing.Size(224, 53);
+            this.btn_AddCat.Size = new System.Drawing.Size(168, 43);
             this.btn_AddCat.TabIndex = 17;
             this.btn_AddCat.Text = "Add";
-            this.btn_AddCat.UseVisualStyleBackColor = true;
+            this.btn_AddCat.UseVisualStyleBackColor = false;
             this.btn_AddCat.Click += new System.EventHandler(this.btn_AddCat_Click);
             // 
             // btn_AddCatback
             // 
+            this.btn_AddCatback.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.btn_AddCatback.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_AddCatback.Location = new System.Drawing.Point(62, 445);
+            this.btn_AddCatback.ForeColor = System.Drawing.Color.Yellow;
+            this.btn_AddCatback.Location = new System.Drawing.Point(46, 362);
+            this.btn_AddCatback.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_AddCatback.Name = "btn_AddCatback";
-            this.btn_AddCatback.Size = new System.Drawing.Size(194, 49);
+            this.btn_AddCatback.Size = new System.Drawing.Size(146, 40);
             this.btn_AddCatback.TabIndex = 18;
             this.btn_AddCatback.Text = "Back";
-            this.btn_AddCatback.UseVisualStyleBackColor = true;
+            this.btn_AddCatback.UseVisualStyleBackColor = false;
             this.btn_AddCatback.Click += new System.EventHandler(this.btn_AddCatback_Click);
             // 
             // AddCategory
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(901, 518);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(676, 421);
             this.Controls.Add(this.btn_AddCatback);
             this.Controls.Add(this.btn_AddCat);
             this.Controls.Add(this.lbl_addCatID);
@@ -116,6 +132,7 @@
             this.Controls.Add(this.txt_addCatName);
             this.Controls.Add(this.txt_addCatID);
             this.Controls.Add(this.lbl_AddCattitle);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "AddCategory";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "AddCategory";
--- DB Project/DB Project/MainPage.Designer.cs
+++ DB Project/DB Project/MainPage.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainPage));
             this.lbl_title = new System.Windows.Forms.Label();
             this.btn_customer = new System.Windows.Forms.Button();
             this.btn_sysAdmin = new System.Windows.Forms.Button();
@@ -35,44 +36,53 @@
             // 
             // lbl_title
             // 
-            this.lbl_title.Font = new System.Drawing.Font("Book Antiqua", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_title.Location = new System.Drawing.Point(276, 61);
+            this.lbl_title.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_title.Font = new System.Drawing.Font("Book Antiqua", 30.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_title.Location = new System.Drawing.Point(32, 23);
             this.lbl_title.Name = "lbl_title";
-            this.lbl_title.Size = new System.Drawing.Size(224, 77);
+            this.lbl_title.Size = new System.Drawing.Size(192, 62);
             this.lbl_title.TabIndex = 1;
             this.lbl_title.Text = "Homely";
             this.lbl_title.Click += new System.EventHandler(this.label1_Click);
             // 
             // btn_customer
             // 
+            this.btn_customer.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.btn_customer.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_customer.Location = new System.Drawing.Point(242, 201);
+            this.btn_customer.ForeColor = System.Drawing.Color.Yellow;
+            this.btn_customer.Location = new System.Drawing.Point(356, 48);
+            this.btn_customer.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btn_customer.Name = "btn_customer";
-            this.btn_customer.Size = new System.Drawing.Size(296, 65);
+            this.btn_customer.Size = new System.Drawing.Size(254, 53);
             this.btn_customer.TabIndex = 2;
             this.btn_customer.Text = "Customer";
-            this.btn_customer.UseVisualStyleBackColor = true;
+            this.btn_customer.UseVisualStyleBackColor = false;
             this.btn_customer.Click += new System.EventHandler(this.btn_customer_Click);
             // 
             // btn_sysAdmin
             // 
+            this.btn_sysAdmin.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.btn_sysAdmin.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_sysAdmin.Location = new System.Drawing.Point(242, 345);
+            this.btn_sysAdmin.ForeColor = System.Drawing.Color.Yellow;
+            this.btn_sysAdmin.Location = new System.Drawing.Point(356, 127);
+            this.btn_sysAdmin.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btn_sysAdmin.Name = "btn_sysAdmin";
-            this.btn_sysAdmin.Size = new System.Drawing.Size(296, 65);
+            this.btn_sysAdmin.Size = new System.Drawing.Size(254, 53);
             this.btn_sysAdmin.TabIndex = 3;
             this.btn_sysAdmin.Text = "System Admin";
-            this.btn_sysAdmin.UseVisualStyleBackColor = true;
+            this.btn_sysAdmin.UseVisualStyleBackColor = false;
             this.btn_sysAdmin.Click += new System.EventHandler(this.btn_sysAdmin_Click);
             // 
             // MainPage
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(788, 518);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(675, 421);
             this.Controls.Add(this.btn_sysAdmin);
             this.Controls.Add(this.btn_customer);
             this.Controls.Add(this.lbl_title);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "MainPage";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "                 ";
